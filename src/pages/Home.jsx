df90d92b491e932ebdf737409dd4c8311c0368cd
Krisna Firdaus <krisnafirdaus@gmail.com>
2025-08-16 14:40:25 +0700

    feat: text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Button from '../components/Button.jsx';
+import Text from '../components/Text.jsx';
 
 const Home = () => {
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <Button />
+      <Text />
     </div>
   );
 };
