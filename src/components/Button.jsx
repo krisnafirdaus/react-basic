f4a880a87057cb6720d5a1fa1885c223d4035303
Krisna Firdaus <krisnafirdaus@gmail.com>
2025-08-16 14:26:45 +0700

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+const Button = () => {
+    return (
+        <>
+            <button>Click me</button>
+        </>
+    )
+}
+
+export default Button;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Button from '../components/Button.jsx';
+
+const Home = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default Home;
